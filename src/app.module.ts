ef72a6f2a0b523a50572afe9bbbd3d89f941a2ff
SERDAR DURDYEV <sdurdiev@code-pilots.ru>
2021-03-12 12:07:04 +0400

    Add good crud controller & service but not tested with memcached

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
+import { GoodModule } from './good/good.module';
 
 @Module({
   imports: [
