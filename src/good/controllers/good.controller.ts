d92357cbae3b4ebd01af81555577fd497a70f67e
SERDAR DURDYEV <sdurdiev@code-pilots.ru>
2021-03-18 15:15:54 +0400

    Add good dto controller validation

--- src/good/controllers/good.controller.ts
+++ src/good/controllers/good.controller.ts
@@ -8,15 +8,22 @@ import { Crud } from '@nestjsx/crud';
 import { Good } from '../entity/good.entity';
 import { JwtAuthGuard } from '../../auth/guards/jwt.guard';
 import { GoodService } from '../services/good.service';
+import { GoodCreateDto } from '../dto/good.create.dto';
+import { GoodUpdateDto } from '../dto/good.update';
 
 @ApiTags('goods')
 @Crud({
   model: {
     type: Good,
   },
+  dto: {
+    create: GoodCreateDto,
+    update: GoodUpdateDto,
+  },
   routes: {
     createOneBase: {
       decorators: [UseGuards(JwtAuthGuard)],
+
     },
     updateOneBase: {
       decorators: [UseGuards(JwtAuthGuard)],
