ff1c09b2cc6de68f72cad0b42172daf4a2425667
SERDAR DURDYEV <sdurdiev@code-pilots.ru>
2021-03-19 13:02:02 +0400

    Refactor redis io adapter

--- src/common/adapters/redis-io-adapter.ts
+++ src/common/adapters/redis-io-adapter.ts
@@ -1,19 +1,22 @@
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ConfigService } from '@nestjs/config';
 import * as redisIoAdapter from 'socket.io-redis';
+import { INestApplication } from '@nestjs/common';
 
 export class RedisIoAdapter extends IoAdapter {
-  private configService: ConfigService = new ConfigService();
+  private configService: ConfigService<Record<string, any>>;
 
+  constructor(app: INestApplication) {
+    super(app);
+    this.configService = new ConfigService();
+  }
 
   createIOServer(port: number, options?: any): any {
-    const server = super.createIOServer(port, options);
-    // @ts-ignore
     const redisAdapter = redisIoAdapter({
       host: this.configService.get('REDIS_HOST'),
       port: this.configService.get('REDIS_PORT'),
     });
-
+    const server = super.createIOServer(port, options);
     server.adapter(redisAdapter);
     return server;
   }
